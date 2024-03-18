4b2e991beaf6a7a17057de5366f2a88e7d651f74
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-03-18 17:13:23 -0500

    set state for name, description, and time

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import './App.css'
 
 function App() {
+  const [name, setName] = useState(initialState,'');
+  const [datetime, setDatetime] = useState(initialState,'');
+  const [description, setDescription] = useState(initialState,'');
+  function addNewTransaction() {
+    
+  }
   return (
     <main>
       <h1>$400<span>.00</span></h1>
       
-      <form>
+      <form onSubmit={addNewTransaction}>
         <div className="basic">
-          <input type="text" placeholder={'-$200 new TV'}/>
-          <input type="datetime-local"/>
+          <input type="text" value={name} onChange={ev => setName(ev.target.value)} placeholder={'-$200 new TV'}/>
+          <input type="datetime-local" value={datetime} onChange={ev => setDatetime(ev.target.value)}/>
         </div>
         <div className="description">
-          <input type="text" placeholder={'description'}/>
+          <input type="text" value={description} onChange={ev => setDescription(ev.target.value)} placeholder={'description'}/>
         </div>
           <button type="submit">Add Transaction</button>
         </form>
